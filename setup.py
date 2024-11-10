3b2e5fd2120c8a7870e9fb7bf072dfda6299e2eb
ellie_ygt <yghelnaz@gmail.com>
2024-11-10 15:27:38 +0100

    Some codes have been reviewed

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="math_quiz",  # This should be unique to avoid conflicts on PyPI
+    name="math_quiz", 
     version="0.1",
     packages=find_packages(),
-    install_requires=[],  # Add any dependencies here or specify in requirements.txt
+    install_requires=[],  
 
     # Metadata
     author="Elnaz Yaghouti",
     author_email="yghelnaz@gmail.com",
     description="A simple math quiz game",
     license="Apache License 2.0",
-    url="https://github.com/elnazyaghouti/dsss_homework_2",  # Replace with your repo link
+    url="https://github.com/elnazyaghouti/dsss_homework_2",  
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
@@ -19,7 +19,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'math_quiz=math_quiz.math_quiz:math_quiz',  # This allows running `math_quiz` from the terminal
+            'math_quiz=math_quiz.math_quiz:math_quiz',  
         ],
     },
 )
